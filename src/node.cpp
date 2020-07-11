2227b618f7d7c0e38820dc90a0f3f89373abd61f
Mikhail Fedyakov <mf_52@mail.ru>
2020-07-11 13:37:56 +0300

    added cmake file

---
+++ src/node.cpp
@@ -0,0 +1,12 @@
+#include "node.h"
+
+template<class T>
+Node<T>::Node(T const& data, Node* next, Node* prev) {
+    this->next = next;
+    this->prev = prev;
+    this->data = data;
+}
+
+template<class T>
+Node<T>::~Node() {
+}
